1d6ac231e5679e6e7ea02eadbf3ba32aa83c4827
o220s <pjs47792332@gmail.com>
2024-06-13 16:27:32 +0900

    next 포트폴리오 작업

---
+++ src/app/projects/page.js
@@ -0,0 +1,12 @@
+import Footer from "../../../components/footer";
+import Header from "../../../components/header";
+
+export default function Projects() {
+  return (
+    <div>
+      <Header />
+      <h1>Projects</h1>
+      <Footer />
+    </div>
+  );
+}
