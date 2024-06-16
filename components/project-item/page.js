28c7194ffd780fd713822be3238e4c7218482253
o220s <pjs47792332@gmail.com>
2024-06-17 01:15:02 +0900

    feat : 커버이미지 경로 변경

--- components/project-item/page.js
+++ components/project-item/page.js
@@ -7,9 +7,7 @@ export default function ProjectItem({ data }) {
   const description =
     data.properties.설명.rich_text[0]?.plain_text || "설명 없음";
   const githubLink = data.properties.GitHub.url;
-  const imgSrc = data.cover.file
-    ? data.cover.file.url
-    : data.cover.external.url;
+  const imgSrc = data.cover.file.url || data.cover.external.url;
   const tags = data.properties.Tags.multi_select;
   const start = data.properties.날짜.date.start;
   const end = data.properties.날짜.date.end;
